aea0b808d2ae396e9e175af8a0cde73726bff310
Ariel10b <burgosariel27@gmail.com>
2025-07-26 12:57:32 -0500

    Implementacion de Registro y Acceso

--- src/App.java
+++ src/App.java
@@ -1,8 +1,13 @@
 import LectorTarjeta.ControllerLector;
+import RegistroAcceso.GestorAccesos;
 
 public class App {
     public static void main(String[] args) {
+
         ControllerLector lector = new ControllerLector();
         lector.startRead();
+
+        GestorAccesos gestorAccesos = new GestorAccesos();
+        gestorAccesos.mostrarAccesos();
     }
 }
