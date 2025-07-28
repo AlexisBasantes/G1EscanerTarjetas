39c7499ef1ba5da7761a1b98ea054ac47f5f1d95
AlexisBasantes <alexisbasantes1017@gmail.com>
2025-07-28 11:40:23 -0500

    Correccion en el merge Co-authored-by: Anasi Eduardo danny.anasi@epn.edu.ec, Cadena Steve: stevencadena12@gmail.com

--- src/db/ConexionSQLite.java
+++ src/db/ConexionSQLite.java
@@ -1,3 +1,25 @@
-public class ConexionSQLite {
+package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionSQLite {
+    private static final String URL = "jdbc:sqlite:src/bd/estudiantes.db";
+
+    /**
+     * Establece una conexión a la base de datos SQLite.
+     *
+     * @return Connection objeto de conexión a la base de datos.
+     */
+    public static Connection conectar() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(URL);
+            System.out.println("Conexión exitosa a SQLite.");
+        } catch (SQLException e) {
+            System.out.println("Error al conectar con SQLite: " + e.getMessage());
+        }
+        return conn;
+    }
 }
